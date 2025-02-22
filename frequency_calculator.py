d0927d209cfec71f35763bdd17bb3de3f5146c1d
Teonord <teo.nordstrom@outlook.com>
2025-02-22 14:41:52 +0100

    fix: fixed flipped name of func
    Fixed volume from frequency function being called frequency from volume

--- frequency_calculator.py
+++ frequency_calculator.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def freq_from_vol(coefficients: list[float], freqs: list[float]) -> list[list[float]]:
+def vol_from_freq(coefficients: list[float], freqs: list[float]) -> list[list[float]]:
     last = coefficients[-1]
 
     res = []
@@ -20,11 +20,11 @@ def freq_from_vol(coefficients: list[float], freqs: list[float]) -> list[list[fl
 
 
 if __name__ == "__main__":
-    print(freq_from_vol(
+    print(vol_from_freq(
         [7.51933578e-10, -4.69617180e-07, 8.81391937e-05, -9.88539231e-03, 2.87005924e-01, 8.49848104e+02],
         [440, 466.164, 493.883, 523.251, 554.365, 587.33, 622.256, 659.255, 698.456, 739.989, 783.991, 830.609]))
 
-    print(freq_from_vol(
+    print(vol_from_freq(
         [1.70256556e-09, -1.35671460e-06, 3.56977104e-04, -3.88252866e-02, 1.31541612e+00, 6.72717358e+02],
         [277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305,
          440, 466.164, 493.883, 523.251, 554.365, 587.33, 622.254, 659.255]))
