47bb7e8fa8c6f8547dcdc182fffa020dc9bf7606
Teonord <teo.nordstrom@outlook.com>
2025-02-23 16:32:54 +0100

    feat: added partials and rotation envelope
    Added generator for partials and envelope for the sound of the finger rotation

--- wineglass.py
+++ wineglass.py
@@ -1,11 +1,16 @@
 import numpy as np
+import sounddevice
+from scipy.signal import butter, filtfilt
+
 from curvefit import model, wg1_coefficients, wg2_coefficients
 
 
 GLASS_ONE: bool = True
-FINGER_RPM: float = 60
+FINGER_RPM: float = 40
 WATER_VOLUME_ML: float = 152.786
-SUSTAIN_DURATION: float = 10
+SUSTAIN_DURATION: float = 3
+SAMPLE_RATE: int = 41100
+PARTIALS_NUM: int = 10
 
 
 # MIDI semitones to frequency
@@ -28,7 +33,45 @@ def a_to_db(a: float) -> float:
     return 20 * np.log10(a)
 
 
-coefficients = wg1_coefficients() if GLASS_ONE == 1 else wg2_coefficients()
+#  Simple amplitude normalization
+def normalize_audio(sound: np.ndarray):
+    return sound / max(np.abs(sound))
 
-f0 = model(WATER_VOLUME_ML, *coefficients)
+
+# Create a partial
+def generate_partial(time: np.ndarray, frequency: float):
+    return np.sin(2 * np.pi * frequency * time)
+
+
+# Make the amplitude oscillation
+def rotation_envelope(sound: np.ndarray, time: np.ndarray, rpm: float, depth: float = 0.8) -> np.ndarray:
+    nps = rpm / 15  # nodes per second, 4 per rotation
+    sound *= np.abs(np.sin(np.pi * nps * time)) * depth + 1 - depth  # 2 nodes per wavelength due to abs
+    return sound
+
+
+def main():
+    coefficients = wg1_coefficients() if GLASS_ONE == 1 else wg2_coefficients()
+
+    f0 = model(WATER_VOLUME_ML, *coefficients[0])
+
+    note_time = np.linspace(0, SUSTAIN_DURATION, int(SAMPLE_RATE * SUSTAIN_DURATION))
+
+    sound = generate_partial(note_time, f0)
+    sound = rotation_envelope(sound, note_time, FINGER_RPM)
+
+    for i in range(2, PARTIALS_NUM + 1):
+        partial = generate_partial(note_time, f0 * i) * (1 / (i * 2)) ** 3
+        sound += partial
+
+    sound = normalize_audio(sound)
+
+    sounddevice.play(sound, SAMPLE_RATE)
+    sounddevice.wait()
+
+
+if __name__ == "__main__":
+    main()
+
+
 
